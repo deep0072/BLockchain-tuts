75398d71b8b63881425586d4d05f1c9e6120d05d
unknown <deepakumar50136@gmail.com>
2022-05-25 12:41:04 +0530

    pure view function details

---
+++ function/view_pure_func.sol
@@ -0,0 +1,27 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Mycontract {
+    uint256 public x = 98;
+
+    function Myadd(uint256 y) external view returns (int256) {
+        return x + y;
+    }
+
+    function Mysub(uint256 z, uint256 y) external pure returns (uint256) {
+        return z + y;
+    }
+
+    // invalid view function
+    function Myadd(uint256 y) external view returns (uint256) {
+        return x += y;
+
+        // x already declared and we are trying to modifying the current x so it is invalid
+    }
+
+    // invalid pure function
+    function Myadd(uint256 y) external pure returns (uint256) {
+        return x + y;
+
+        // this is invalid because pure function cant access the already declare variable
+    }
+}
