d9b9c329b54671fab46c01d7dbbce4b02a92964b
unknown <deepakumar50136@gmail.com>
2022-05-25 16:41:38 +0530

    default values of all variable

---
+++ variable/defaultValues.sol
@@ -0,0 +1,9 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract defaultValues {
+    bool public b; // false
+    int256 public i; // 0
+    uint256 public u; // 0
+    address public addr; // 0X00000000000000000000000000000000
+    bytes32 public b32;
+} // 0x00000000000000000000000000000000
