2c5250a9d1196c0bc277a9da27b89d0065696e1c
unknown <deepakumar50136@gmail.com>
2022-05-26 13:24:41 +0530

    constant variable

---
+++ variable/const.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Constant {
+    uint256 public constant X = 78;
+    //execution cost	21371 gas
+}
+
+contract Var {
+    uint256 public x = 89;
+    //execution cost	23471 gas
+}
