16f73089f28f695005eaefb1f349961320b05c44
unknown <deepakumar50136@gmail.com>
2022-06-11 20:18:10 +0530

    calling contract into aanother contract

---
+++ solidityprogram/callAnothercontract.sol
@@ -0,0 +1,30 @@
+pragma solidity >=0.7.5 <=0.8.9;
+
+contract myAccount {
+    address public bank;
+    address public owner;
+
+    constructor(address _owner) payable {
+        bank = msg.sender;
+        owner = _owner;
+    }
+}
+
+/*
+
+here ContractFactory creating another "myAccount" contract and sending ether to it 
+
+in "myAccount" bank store the  the address of contractFactory and owner is the address who deploying the ContractFactory
+
+
+ */
+
+contract ContractFactory {
+    myAccount[] public accounts;
+
+    function createAccount(address _owner) external payable {
+        myAccount account = new myAccount{value: 111}(_owner); // calling another contract using new followed by contract name
+
+        accounts.push(account);
+    }
+}
