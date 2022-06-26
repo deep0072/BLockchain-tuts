39e6b7750c6914a4770573a2837462969bfdc60a
unknown <deepakumar50136@gmail.com>
2022-06-26 17:16:19 +0530

    erc 20 functions added and read me also updated

--- ERC20functions/erc20.sol
+++ ERC20functions/erc20.sol
@@ -1,55 +1,98 @@
 pragma solidity >=0.7.5 <=0.8.7;
 
-contract ERC20 {
-    event TrasferToken(address _sender, address _Reciever, uint256 amount);
+contract Myerc20 {
+    event transferToken(address sender, address reciever, uint256 amount);
+    event allowedInfo(address from, address reciever, uint256 amount);
 
-    string public tokeName;
-    string public tokenSymbol;
+    mapping(address => mapping(address => uint256)) allowed;
 
+    address owner;
+
+    string public tokenName;
+    string public symbol;
     uint256 public totalSupply;
-    uint256 public circulatedSupply;
-
-    mapping(address => uint256) public balances;
-    address public owner; 
+    mapping(address => uint256) balances;
 
     constructor(
-        ;
-        string memory _tokeName,
-        string memory _tokenSymbol,
-        uint256 _totalSupply
+        string memory _tokenName,
+        uint256 _totalSupply,
+        string memory _symbol
     ) {
-        tokenSymbol = _tokeName;
-        tokenSymbol = _tokenSymbol;
+        tokenName = _tokenName;
         totalSupply = _totalSupply;
-        balances[msg.sender] = totalSupply;
+        symbol = _symbol;
+
+        owner = msg.sender;
+        balances[msg.sender] += _totalSupply;
     }
 
-    modifier OnlyAdmin() {
-        require(msg.sender == owner, "you are not authorised");
+    modifier OnlyAdmin(address addr) {
+        require(addr == owner, " not authorised");
         _;
     }
 
-    function transfer(address reciever, uint256 amount) external {
-        require(amount <= balances[msg.sender], "not sufficient amount");
-
-        balances[reciever] += amount;
-        balances[msg.sender] -= amount;
-        emit TrasferToken(msg.sender, reciever, amount);
-    }
-
-    function balanceOf(address _addr) external views returns (uint256) {
+    function blancesOf(address _addr) external view returns (uint256) {
         return balances[_addr];
     }
 
-    function _mint(uint256 _amount) external OnlyAdmin{
-        totalSupply += _amount;
-        balances[msg.sender]+=_amount
+    function transfer(uint256 amount, address _to) external {
+        require(amount <= balances[msg.sender], "not sufficient amount");
 
+        balances[_to] += amount;
+        balances[msg.sender] -= amount;
+        emit transferToken(msg.sender, _to, amount);
     }
 
+    function _mint(uint256 amount) external OnlyAdmin(msg.sender) {
+        totalSupply += amount;
+        balances[msg.sender] += amount;
+    }
 
-    function burn(uint _amount) external {
-        total_supply -= _amount;
-        balances["msg.sender"] -= _amount;
+    function burn(address burner, uint256 amount) external {
+        require(amount <= balances[burner], " not sufficient amount to burn");
+        balances[burner] -= amount;
+        totalSupply -= amount;
+    }
+
+    // check the allowed token balance
+    function allowance(
+        address _owner,
+        address _spender,
+        uint256 token
+    ) external view returns (uint256) {
+        return allowed[_owner][_spender];
+    }
+
+    // to approve the token to spend
+    function approve(
+        address _owner,
+        address _spender,
+        uint256 amount
+    ) external OnlyAdmin(msg.sender) {
+        require(
+            amount <= balances[_owner],
+            "not sufficient amount for allowance"
+        );
+
+        allowed[_owner][_spender] += amount;
+
+        emit allowedInfo(_owner, _spender, amount);
+    }
+
+    // function to spend the alowed token here only spender allowed to spend the token. msg.sender is spender
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 amount
+    ) external {
+        require(
+            amount <= allowed[_from][msg.sender] && amount <= balances[_from],
+            "not sufficent amount to spend"
+        );
+        balances[_from] -= amount;
+        allowed[_from][msg.sender] -= amount;
+
+        emit transferToken(_from, _to, amount);
     }
 }
--- README.md
+++ README.md
@@ -317,6 +317,12 @@
 
 ### transfer() ==> This function lets the owner of the contract send a given amount of the token to another address just like a conventional cryptocurrency transaction
 
+### approve function ==>  This function lets the owner of the contract approve a given amount of tokens to be spent on behalf of another address.means  lets say i am the owner of wallet and i decide whoever want to  spend my tokne. i will allow them to spend my token. by giving approval. i will set the limit that the other wallet spend my token.
+
+
+### allowance function ==> This function lets the owner of the contract check the amount of tokens that have been approved for spending on behalf of another address.menas if i am the owner of my wallet and my friend has  spend my token on behalf of my wallet. so this function to check how much allowance is remaining for my friend.
+
+### transferFrom function ==> This function lets the owner of the contract transfer a given amount of tokens from one address to another address.one address corresponds to the who actual have the token and other address is the one who want to spend the token. transferFrom function is used to spend the token on behalf of the owner of the contract.
 
 
 
