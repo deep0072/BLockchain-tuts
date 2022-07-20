5e303a8e9b31b22fd4b7c1890e04276fba60d701
unknown <deepakumar50136@gmail.com>
2022-07-20 23:27:00 +0530

    comment removed

--- fundmeProject/fundme.sol
+++ fundmeProject/fundme.sol
@@ -3,8 +3,6 @@ pragma solidity ^0.8.5;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./priceConvert.sol";
 
-//	938610 gas
-//916123 gas
 
 contract Fundme {
     using priceConvert for uint256;
