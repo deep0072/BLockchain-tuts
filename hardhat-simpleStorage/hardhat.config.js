1b674cbf974d6b6c1eb6b222ffe3d4214d10d88a
unknown <deepakumar50136@gmail.com>
2022-08-02 17:34:09 +0530

    verifying code

--- hardhat-simpleStorage/hardhat.config.js
+++ hardhat-simpleStorage/hardhat.config.js
@@ -1,8 +1,10 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
 
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
 const RINKEBY_PRIVATE_KEY = process.env.private_key;
+const Etherscan_API_KEY = process.env.etherscan_api_key;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -15,4 +17,9 @@ module.exports = {
     },
   },
   solidity: "0.8.9",
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: Etherscan_API_KEY,
+  },
 };
--- hardhat-simpleStorage/scripts/deploy.js
+++ hardhat-simpleStorage/scripts/deploy.js
@@ -4,13 +4,33 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const { ethers } = require("hardhat");
+const { ethers, run } = require("hardhat");
 
 async function main() {
   const simpleStorage = await ethers.getContractFactory("SimpleStorage");
   const SimpleStorageDeploy = await simpleStorage.deploy();
   await SimpleStorageDeploy.deployed();
   console.log(`deployed contract to ${SimpleStorageDeploy.address}`);
+}
+
+// verify deployed contract
+async function verify(conractAddress, args) {
+  console.log("deploying");
+
+  // to check the deployed contract is verified or not
+  // also to handle the error during publications
+  try {
+    await run("verify: verify", {
+      address: conractAddress,
+      constructorArguments: args,
+    });
+  } catch (e) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("verified ");
+    } else {
+      console.log(e);
+    }
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
