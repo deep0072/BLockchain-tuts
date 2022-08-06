62d057616dd6638964708929756216644cdbe4dc
unknown <deep0072>
2022-08-07 00:54:59 +0530

    now get the smartcontract deployment gas fees in dollar also

--- hardhat-simpleStorage/hardhat.config.js
+++ hardhat-simpleStorage/hardhat.config.js
@@ -7,6 +7,7 @@ require("hardhat-gas-reporter");
 const Etherscan_API_KEY = process.env.Etherscan_API_KEY;
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
 const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+const COINMARKETCAP =   process.env.COINMARKETCAP
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
@@ -27,6 +28,10 @@ module.exports = {
   solidity: "0.8.9",
   etherscan: { apiKey: Etherscan_API_KEY },
   gasReporter:{
-    enabled:true
+    enabled:true,
+    outputFile: "gasReports.txt",
+    noColors:true,
+    currency:"USD",
+    coinmarketcap: COINMARKETCAP
   }
 };
