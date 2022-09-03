782472694c98cf550f150f7da8d31be4f45f3d64
unknown <deep0072>
2022-09-03 15:04:21 +0530

    withdraw functionality added

--- html-fundme-hardhat/index.js
+++ html-fundme-hardhat/index.js
@@ -3,9 +3,12 @@ import { abi, contractAddress } from "./constants.js";
 
 const connectButton = document.getElementById("connectButton");
 const fundButton = document.getElementById("fundButton");
+const getbalance = document.getElementById("balanceButton");
+const withdraw = document.getElementById("withDrawButton");
+getbalance.onclick = getBalance;
 connectButton.onclick = connect;
 fundButton.onclick = Fund;
-
+withdraw.onclick = withDraw;
 async function connect() {
   if (typeof window.ethereum !== "undefined") {
     await window.ethereum.request({ method: "eth_requestAccounts" });
@@ -16,8 +19,18 @@ async function connect() {
   }
 }
 
+async function getBalance() {
+  if (typeof window.ethereum !== "undefined") {
+    const provider = new ethers.providers.Web3Provider(window.ethereum); // window.ethereumwindow. ethereum . This API allows websites to request
+    // users' Ethereum accounts, read data from blockchains the user is connected to
+    const balance = await provider.getBalance(contractAddress); // now getbalance of mentioned address in method
+    console.log(ethers.utils.formatEther(balance));
+  }
+}
+
 async function Fund() {
   const ethAmount = document.getElementById("ethAmount").value;
+  console.log("ethAmount", ethAmount);
   if (typeof window.ethereum != "undefined") {
     console.log("funding.....");
     // get connection to blockchain with help of provider
@@ -42,6 +55,22 @@ async function Fund() {
   }
 }
 
+async function withDraw() {
+  if (typeof window.ethereum !== "undefined") {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = await provider.getSigner()
+    const contract = new ethers.Contract(contractAddress, abi,signer)
+    try{
+      const transactionResponse = await contract.withdraw()
+      //then wait for transaction confirmations using listenForTransaction
+      await listenForTransaction(transactionResponse, provider)
+    }catch (error){
+      console.log(error)
+    }
+
+  }
+}
+
 function listenForTransaction(transactionResponse, provider) {
   console.log(`Mining ${transactionResponse.hash}.............`);
   // now listen this transaction from blockhain
@@ -49,8 +78,7 @@ function listenForTransaction(transactionResponse, provider) {
   return new Promise((resolve, reject) => {
     provider.once(transactionResponse.hash, (transReceipt) => {
       console.log(`Completed with ${transReceipt.confirmations} confirmations`);
-      resolve()
+      resolve();
     });
-    
   });
 }
