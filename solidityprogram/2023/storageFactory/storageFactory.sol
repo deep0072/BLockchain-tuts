a123dff24b977c3cb17ae862904bfcfdfc7c3d0e
deep0072 <deepakumar50136@gmail.com>
2023-06-10 14:14:49 +0530

    deployed contrat from another contract, access all function from another contract

--- solidityprogram/2023/storageFactory/storageFactory.sol
+++ solidityprogram/2023/storageFactory/storageFactory.sol
@@ -5,10 +5,29 @@ import {SimpleStorage} from "./simpleStorage.sol"; // import specific contract
 
 contract StorageFactory {
     // intializing the contract type variable here
-    SimpleStorage public myListOfSimpleStorage;
+    SimpleStorage[] public myListOfSimpleStorage;
 
     function createSimpleStorageContract() public {
         SimpleStorage mySimpleStorage = new SimpleStorage(); // here we are trying to deploy contract here
         myListOfSimpleStorage.push(mySimpleStorage);
+    }
+
+    function sfStore(
+        uint256 _indexSimplestorage,
+        uint256 _simpleStorageNumber
+    ) public {
+        // get deployed contract address
+
+        // then  use deployed contract function to set new number
+        myListOfSimpleStorage[_indexSimplestorage].setFavoriteNumber(
+            _simpleStorageNumber
+        );
+    }
+
+    // get favnumber from selected
+    function retreiveNumber(
+        uint256 _simpleStorageIndex
+    ) public view returns (uint256) {
+        return myListOfSimpleStorage[_simpleStorageIndex].getMyFavNumber();
     }
 }
